100b19ff1e99d245b5b3d1bbb8d71e0d5b6c45f5
fabrizhio al chariti <fabrizhioal.2011@gmail.com>
2022-11-19 11:02:24 -0400

    Cambios y area de subadministradores

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
 
-
 //pages
 
 import Users from "./pages/Users";
 import Public from "./pages/Public";
 
 function App() {
-
   return (
     <>
-      
       <Routes>
         <Route path="/*" end element={<Public />} />
-        <Route path="/users/*" element={<Users/>}/>
+        <Route path="/users/*" element={<Users />} />
       </Routes>
-    
     </>
   );
 }
 
 export default App;
-
-
-
-
